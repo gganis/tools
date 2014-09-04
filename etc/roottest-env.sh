d091be875dc26fc3d492fa2a41a992d4890e335a
Gerardo Ganis <gerardo.ganis@cern.ch>
2014-09-04 12:12:00 +0200

    Add tools to setup roottest

---
+++ etc/roottest-env.sh
@@ -0,0 +1,25 @@
+#
+# Source this to set all what you need to use roottest at <roottest_path> 
+#
+# Usage:
+#            source /Path/to/setroottest.sh <roottest_path>
+#
+#
+roottest_dir=""
+dirs="$1 $HOME/local/roottest/$1"
+for d in $dirs ; do
+   if test -d $d ; then
+      roottest_dir="$d"
+      break
+   fi
+done
+
+export ROOTTEST_HOME="$roottest_dir"
+
+echo "Using roottest at $roottest_dir"
+
+if test "x$2" = "xcd" ; then
+   cd $roottest_dir
+fi
+
+
