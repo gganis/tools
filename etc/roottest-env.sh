40ed88dc29d9dc92bd492ee80040e9679a0346a3
Gerardo Ganis <gerardo.ganis@cern.ch>
2014-09-04 15:17:37 +0200

    Add optional directory creation

--- etc/roottest-env.sh
+++ etc/roottest-env.sh
@@ -7,21 +7,62 @@
 #
 set `echo "$1"`
 
-roottest_dir=""
-dirs="$1 $HOME/local/roottest/$1"
-for d in $dirs ; do
-   if test -d $d ; then
-      roottest_dir="$d"
-      break
+cdtodir="no"
+rd="$PWD"
+if test ! "x$1" = "x" ; then
+
+  if test "x$1" = "xcd" ; then
+     cdtodir="yes"
+  else
+     rd="$1"
+     if test "x$2" = "xcd" ; then
+        cdtodir="yes"
+     fi
+  fi
+
+fi
+
+# Check if there is a request for a sub-dir
+subdir=""
+rddir=`dirname $rd`
+if test "x$rddir" = "x." ; then
+  if test -d "$rd/roottest"; then
+     subdir="$rd/roottest"
+  else
+     subdir="$rd"
+  fi
+else
+  rddirn=`dirname $rddir`
+  if test "x$rddirn" = "x." ; then
+     subdir="$rd"
+  fi
+fi
+if test ! "x$subdir" = "x" ; then
+   rd="$HOME/local/roottest/$subdir"
+fi
+
+pwdsave="$PWD"
+
+if test ! -d $rd ; then
+   docreate="no"
+   echo "Directory $rd does not exist: do you want to create it? (N/y)"
+   read ans
+   if test "x$ans" = "xY" || test "x$ans" = "xy" ; then
+      docreate="yes"
    fi
-done
+   if test "x$docreate" = "xyes" ; then
+      mkdir -p $rd
+   else
+      exit 1
+   fi
+fi
 
-export ROOTTEST_HOME="$roottest_dir"
-
-echo "Using roottest at $roottest_dir"
-
-if test "x$2" = "xcd" ; then
-   cd $roottest_dir
+cd "$rd"
+rd="$PWD"
+echo "Using roottest at $rd"
+export ROOTTEST_HOME="$rd"
+if test "x$cdtodir" = "xno" ; then
+   cd "$pwdsave"
 fi
 
 
