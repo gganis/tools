dc2ce6557485f2e8fa77d736927d25aee4f047a9
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2015-07-30 12:21:12 +0200

    Fix logic

--- etc/cmakeprep.sh
+++ etc/cmakeprep.sh
@@ -7,29 +7,42 @@ fi
 
 TAG=$1
 if test "x$TAG" = "x" ; then
+
    echo "Passing a tag is mandatory!"
    echo "Syntax:"
    echo "   cmakeprep tag"
-   exit 1
-fi
-ROOTLOC="/home/ganis/local/root"
 
-DIRS="$ROOTLOC/cmake/$TAG $ROOTLOC/build/$TAG $ROOTLOC/install/$TAG"
-for dd in $DIRS; do
-   if test ! -d "$dd"; then
-      mkdir -p $dd
+else
+
+   ROOTLOC="/home/ganis/local/root"
+
+   failed="false"
+   DIRS="$ROOTLOC/cmake/$TAG $ROOTLOC/build/$TAG $ROOTLOC/install/$TAG"
+   for dd in $DIRS; do
       if test ! -d "$dd"; then
-         echo "Could not create $dd : quitting"
-         exit 1
+         mkdir -p $dd
+         if test ! -d "$dd"; then
+            echo "Could not create $dd : quitting"
+            failed="true"
+            break
+         fi
       fi
+   done
+
+   if test "x$failed" = "xfalse" ; then
+
+      echo "Going to $ROOTLOC/build/$TAG ..."
+      cd $ROOTLOC/build/$TAG
+      
+      echo "Basic build:           rootcmake"
+      echo "Basic debug build:     rootcmake debug"
+      echo "Basic xrootd build:    rootcmake xrootd"
+      echo "Basic build w/ tests:  rootcmake test"
+
+   else
+
+      echo "problems creating one of the directories ..."
+ 
    fi
-done
-
-echo "Going to $ROOTLOC/build/$TAG ..."
-cd $ROOTLOC/build/$TAG
-
-echo "Basic build:           rootcmake"
-echo "Basic debug build:     rootcmake debug"
-echo "Basic xrootd build:    rootcmake xrootd"
-echo "Basic build w/ tests:  rootcmake test"
-
+fi
+ 
