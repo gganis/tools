3e5afb18cf468f037c4c54635d35bc6b5d80b1e6
Gerardo Ganis <Gerardo.Ganis@cern.ch>
2016-03-04 15:09:42 +0100

    fine tuning

--- etc/setxrd.sh
+++ etc/setxrd.sh
@@ -2,135 +2,116 @@
 # Source this to set all what you need to use Xrootd at <xrd_install_path> 
 #
 # Usage:
-#            source /Path/to/xrd-etc/setxrd.sh <xrd_install_path>
+#      source /Path/to/xrd-etc/setxrd.sh <xrd_install_path>
 #
-#
-xrdsys=""
-dirs="$1 $HOME/local/xrootd/$1/install $HOME/local/xrootd/$1"
-for d in $dirs ; do
-   if test -d $d ; then
-      dd=`basename $d`
-      if test "x$dd" = "x$d" ; then
-         xrdsys="$d/install"
-      else
-         xrdsys="$d"
-      fi
-      break
-   fi
-done
+# Author: Gerardo Ganis
 
-binpath=""
-libpath=""
+drop_from_path()
+{
+   # Assert that we got enough arguments
+   if test $# -ne 2 ; then
+      echo "drop_from_path: needs 2 arguments"
+      return 1
+   fi
+
+   p=$1
+   drop=$2
+
+   newpath=`echo $p | sed -e "s;:${drop}:;:;g" \
+                          -e "s;:${drop};;g"   \
+                          -e "s;${drop}:;;g"   \
+                          -e "s;${drop};;g"`
+}
+
+xrdset="xrdset"
+xrdsys=$1
+xrdbinpath=""
+xrdlibpath=""
 manpath=""
 if test "x$xrdsys" = "x"; then
-   echo "ERROR: specifying the path to the installed distribution is mandatory"
-   echo "       (use absolute path or relative to $HOME/local/xrootd)"
+   echo "$xrdset: ERROR, you must specify the path to the xrootd installed"
    return 1;
 fi
-binpath="$xrdsys/bin"
-if test ! -d "$binpath" ; then
-   echo "ERROR: directory $binpath does not exist or not a directory!"
+xrdbinpath="$xrdsys/bin"
+if test ! -d "$xrdbinpath" ; then
+   echo "$xrdset: ERROR, directory $xrdbinpath does not exist or not a directory!"
    return 1;
 fi
-libpath="$xrdsys/lib"
-if test ! -d "$libpath" ; then
-   libemsg="$libpath"
-   libpath="$xrdsys/lib64"
-   if test ! -d "$libpath" ; then
-      echo "ERROR: directory $libemsg nor $libpath do not exist or are not directories!"
+xrdlibpath="$xrdsys/lib"
+if test ! -d "$xrdlibpath" ; then
+   libemsg="$xrdlibpath"
+   xrdlibpath="$xrdsys/lib64"
+   if test ! -d "$xrdlibpath" ; then
+      echo "$xrdset: ERROR, directory $libemsg nor $xrdlibpath do not exist or not directories!"
       return 1;
    fi
 fi
-manpath="$xrdsys/man"
-if test ! -d "$manpath" ; then
-   manemsg="$manpath"
-   manpath="$xrdsys/share/man"
-   if test ! -d "$manpath" ; then
-      echo "WARNING: directory $manemasg and $manpath do not exist or are not directories; MANPATH unchanged"
-      manpath=""
+xrdmanpath="$xrdsys/man"
+if test ! -d "$xrdmanpath" ; then
+   manemsg="$xrdmanpath"
+   xrdmanpath="$xrdsys/share/man"
+   if test ! -d "$xrdmanpath" ; then
+      echo "$xrdset: WARNING, directory $manemsg and $xrdmanpath do not exist or not directories; MANPATH unchanged"
+      xrdmanpath=""
    fi
-fi
-
-ismac=`uname -s`
-if test "x$ismac" = "xDarwin"; then
-   ismac="yes"
-else
-   ismac="no"
 fi
 
 # Strip present settings, if there
 if test ! "x$XRDSYS" = "x" ; then
-
    # Trim $PATH
-   tpath=""
-   oldpath=`echo $PATH | tr -s ':' ' '`
-   for pp in $oldpath; do
-      if test ! "x$binpath" = "x$pp" ; then
-         if test ! "x$pp" = "x" ; then
-            tpath="$tpath:$pp"
-         fi
-      fi
-   done
+   if [ -n "${PATH}" ]; then
+      drop_from_path $PATH $xrdbinpath
+      PATH=$newpath
+   fi
 
    # Trim $LD_LIBRARY_PATH
-   tldpath=""
-   oldldpath=`echo $LD_LIBRARY_PATH | tr -s ':' ' '`
-   for pp in $oldldpath; do
-      if test ! "x$libpath" = "x$pp" ; then
-         if test ! "x$pp" = "x" ; then
-            tldpath="$tldpath:$pp"
-         fi
-      fi
-   done
+   if [ -n "${LD_LIBRARY_PATH}" ]; then
+      drop_from_path $LD_LIBRARY_PATH $xrdlibpath
+      LD_LIBRARY_PATH=$newpath
+   fi
 
    # Trim $DYLD_LIBRARY_PATH
-   tdyldpath=""
-   if test "x$ismac" = "xyes"; then
-      olddyldpath=`echo $DYLD_LIBRARY_PATH | tr -s ':' ' '`
-      for pp in $olddyldpath; do
-         if test ! "x$libpath" = "x$pp" ; then
-            if test ! "x$pp" = "x" ; then
-               tdyldpath="$tdyldpath:$pp"
-            fi
-         fi
-      done
+   if [ -n "${DYLD_LIBRARY_PATH}" ]; then
+      drop_from_path $DYLD_LIBRARY_PATH $xrdlibpath
+      DYLD_LIBRARY_PATH=$newpath
    fi
 
-   # Trim $MAN_PATH
-   tmanpath=""
-   if test ! "x$manpath" = "x"; then
-      oldmanpath=`echo $MANPATH | tr -s ':' ' '`
-      for pp in $oldmanpath; do
-         if test ! "x$manpath" = "x$pp" ; then
-            if test ! "x$pp" = "x" ; then
-               tmanpath="$tmanpath:$pp"
-            fi
-         fi
-      done
-   fi
-
-else
-
-   # Do not touch
-   tpath="$PATH"
-   tldpath="$LD_LIBRARY_PATH"
-   if test "x$ismac" = "xyes"; then
-      tdyldpath="$DYLD_LIBRARY_PATH"
-   fi
-   if test ! "x$manpath" = "x"; then
-      tmanpath="$MANPATH"
+   # Trim $MANPATH
+   if [ -n "${MANPATH}" ]; then
+      drop_from_path $MANPATH $xrdmanpath
+      MANPATH=$newpath
    fi
 fi
 
-echo "Using XRD at $xrdsys"
+if [ -z "${MANPATH}" ]; then
+   # Grab the default man path before setting the path to avoid duplicates
+   if `which manpath > /dev/null 2>&1` ; then
+      default_manpath=`manpath`
+   else
+      default_manpath=`man -w 2> /dev/null`
+   fi
+fi
+
+#echo "Using XRD at $xrdsys"
 
 export XRDSYS="$xrdsys"
-export PATH="$binpath:$tpath"
-export LD_LIBRARY_PATH="$libpath:$tldpath"
-if test "x$ismac" = "xyes"; then
-   export DYLD_LIBRARY_PATH="$libpath:$tdyldpath"
+if [ -z "${PATH}" ]; then
+   PATH=$xrdbinpath; export PATH
+else
+   PATH=$xrdbinpath:$PATH; export PATH
 fi
-if test ! "x$manpath" = "x"; then
-   export MANPATH="$manpath:$tmanpath"
+if [ -z "${LD_LIBRARY_PATH}" ]; then
+   LD_LIBRARY_PATH=$xrdlibpath; export LD_LIBRARY_PATH
+else
+   LD_LIBRARY_PATH=$xrdlibpath:$LD_LIBRARY_PATH; export LD_LIBRARY_PATH
 fi
-
+if [ -z "${DYLD_LIBRARY_PATH}" ]; then
+   DYLD_LIBRARY_PATH=$xrdlibpath; export DYLD_LIBRARY_PATH       
+else
+   DYLD_LIBRARY_PATH=$xrdlibpath:$DYLD_LIBRARY_PATH; export DYLD_LIBRARY_PATH
+fi
+if [ -z "${MANPATH}" ]; then
+   MANPATH=$xrdmanpath:${default_manpath}; export MANPATH
+else
+   MANPATH=$xrdmanpath:$MANPATH; export MANPATH
+fi
